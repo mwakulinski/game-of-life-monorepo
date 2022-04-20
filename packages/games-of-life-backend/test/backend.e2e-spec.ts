bbc243f620787f595b76337a32ea909e56df952a
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-20 19:59:23 +0200

    implements test: it changes the board after tick method

--- packages/games-of-life-backend/test/backend.e2e-spec.ts
+++ packages/games-of-life-backend/test/backend.e2e-spec.ts
@@ -8,7 +8,7 @@ describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   async function createBoard(board: BoardDto) {
-    await request(app.getHttpServer()).post('/api').send(board).expect(201);
+    await request(app.getHttpServer()).post('/').send(board).expect(201);
   }
 
   const mockBoard = [
@@ -40,17 +40,17 @@ describe('AppController (e2e)', () => {
   it('it should create board with given shape', async () => {
     createBoard({ id: '1', array: mockBoard });
     const { body: response } = await request(app.getHttpServer())
-      .get('/api/1')
+      .get('/1')
       .send();
     expect(response).toEqual(mockBoard);
   });
 
-  // describe('/api/tick (GET)', () => {});
-  // it('it changes the board after tick method', async () => {
-  //   createBoard({ array: mockBoard });
-  //   const { body: response } = await request(app.getHttpServer())
-  //     .get('/api/tick')
-  //     .send();
-  //   expect(response).toEqual(mockBoardAfterTick);
-  // });
+  describe('/api/tick (GET)', () => {});
+  it('it changes the board after tick method', async () => {
+    createBoard({ id: '1', array: mockBoard });
+    const { body: response } = await request(app.getHttpServer())
+      .get('/tick/1')
+      .send();
+    expect(response).toEqual(mockBoardAfterTick);
+  });
 });
