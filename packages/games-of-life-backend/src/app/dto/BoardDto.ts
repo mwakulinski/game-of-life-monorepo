2b585459390c21e9172e6af174b6c1d7b3a274de
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-20 20:42:13 +0200

    add validation

--- packages/games-of-life-backend/src/app/dto/BoardDto.ts
+++ packages/games-of-life-backend/src/app/dto/BoardDto.ts
@@ -1,4 +1,10 @@
+import { IsString, MinLength, IsArray, ValidateNested } from 'class-validator';
+
 export class BoardDto {
+  @IsString()
+  @MinLength(1)
   id: string;
+
+  @IsArray({ each: true })
   array: number[][];
 }
--- packages/games-of-life-backend/src/main.ts
+++ packages/games-of-life-backend/src/main.ts
@@ -1,9 +1,4 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -14,6 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
